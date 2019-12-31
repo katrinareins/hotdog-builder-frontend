60654b510763f8b08b2d1a55ce077799750222f6
learn-co-kat <twothistles@gmail.com>
2019-12-30 21:34:23 -0800

    Neon style for title.

--- index.js
+++ index.js
@@ -304,6 +304,24 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
 
+    // make the truck move
+    // function truckMove() {
+    //     var elem = document.getElementById("food-truck");
+    //     var pos = 0;
+    //     var id = setInterval(frame, 10);
+    //     function frame() {
+    //       if (pos == 350) {
+    //         clearInterval(id);
+    //       } else {
+    //         pos++;
+    //         elem.style.top = pos + 'px';
+    //         elem.style.left = pos + 'px';
+    //       }
+    //     }
+    //   }
+
+    //   truckMove();
+
     
 })
 
